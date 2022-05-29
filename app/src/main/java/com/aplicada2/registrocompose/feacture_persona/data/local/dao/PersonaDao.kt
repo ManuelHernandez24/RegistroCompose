13109c65594f65e44a16276790a2be91bbae2c40
Manuel Hernandez <victor_estevez@ucne.edu.do>
2022-05-29 14:04:57 -0400

    Creación de la Dao Persona

---
+++ app/src/main/java/com/aplicada2/registrocompose/feacture_persona/data/local/dao/PersonaDao.kt
@@ -0,0 +1,25 @@
+package com.aplicada2.registrocompose.feacture_persona.data.local.dao
+
+import android.app.Person
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.aplicada2.registrocompose.feacture_persona.domain.model.Persona
+import  kotlinx.coroutines.flow.Flow
+
+interface PersonaDao {
+
+    @Query("Select * from Persona")
+    fun getAllPersona(): Flow<List<Persona>>
+
+    @Query("Select * from Persona where PersonaId = :personaId")
+    suspend fun getPersonaById(personaId: Int): Persona?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPersona(persona: Persona)
+
+    @Delete
+    suspend fun deletePersona(persona: Persona)
+
+
+
+}
--- app/src/main/java/com/aplicada2/registrocompose/feacture_persona/domain/model/Persona.kt
+++ app/src/main/java/com/aplicada2/registrocompose/feacture_persona/domain/model/Persona.kt
@@ -3,7 +3,7 @@ package com.aplicada2.registrocompose.feacture_persona.domain.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "tabla_persona")
+@Entity
 data class Persona(
     @PrimaryKey(autoGenerate = true)
     val PersonaId: Int,
