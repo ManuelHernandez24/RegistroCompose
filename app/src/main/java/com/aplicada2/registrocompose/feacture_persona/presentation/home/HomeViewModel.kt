3e9bf20eebdf2dc37e7571cce505c413aff92d16
Manuel Hernandez <victor_estevez@ucne.edu.do>
2022-05-31 10:57:43 -0400

    Terminacion del HomeViewModel

--- app/src/main/java/com/aplicada2/registrocompose/feacture_persona/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/aplicada2/registrocompose/feacture_persona/presentation/home/HomeViewModel.kt
@@ -1,15 +1,42 @@
 package com.aplicada2.registrocompose.feacture_persona.presentation.home
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.aplicada2.registrocompose.feacture_persona.domain.use_cases.DeletePersona
 import com.aplicada2.registrocompose.feacture_persona.domain.use_cases.GetAllPersona
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val deletePersona: DeletePersona,
-    private val getAllPersona: GetAllPersona,
+    getAllPersona: GetAllPersona
 ): ViewModel() {
+
+    private val _state = mutableStateOf(HomeState())
+    val state: State<HomeState> = _state
+
+    init {
+        getAllPersona().onEach { personas ->
+        _state.value = state.value.copy(
+            personas = personas
+        )
+        }.launchIn(viewModelScope)
+    }
+
+    fun onEven(event: HomeEvent){
+        when (event){
+            is HomeEvent.DeletePersona -> {
+                viewModelScope.launch {
+                    deletePersona(event.persona)
+                }
+            }
+        }
+    }
 
 }
