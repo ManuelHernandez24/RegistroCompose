4f80ea0dbff4bb396ca974a473ad0aa89159160f
Manuel Hernandez <victor_estevez@ucne.edu.do>
2022-05-31 10:04:27 -0400

    Creando el HomeState

---
+++ app/src/main/java/com/aplicada2/registrocompose/feacture_persona/presentation/home/HomeState.kt
@@ -0,0 +1,7 @@
+package com.aplicada2.registrocompose.feacture_persona.presentation.home
+
+import com.aplicada2.registrocompose.feacture_persona.domain.model.Persona
+
+class HomeState {
+    val Personas: List<Persona> = emptyList()
+}
